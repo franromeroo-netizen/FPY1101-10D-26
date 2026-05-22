69c8398259fdc8c493af0dd4968eee3239522a52
franromeroo-netizen <you@example.com>
2026-05-22 16:48:37 -0400

    funciones

--- Menú.py
+++ Menú.py
@@ -1,27 +1,32 @@
-DEUDA = 100000
+import casdasda as fn
 
-i = True
-while i:
-    print("""Menú
-1. Pago deuda Tarjeta de crédito
-2. Simulación de compras
-3. Salir""")
-    op_menu = int(input("--> "))
 
-    if op_menu == 1:
-        monto = int(input("Ingrese Monto para pagar deuda\n--> "))
-        if monto > 100000:
-            print("El monto excede la deuda, felicidades")
-        elif monto == 100000:
-            print("El monto paga la deuda, felicidades")
+menu = True
+while menu:
+    print("""Menú de prueba para funciones
+1. sumar
+2. restar
+3. salir""")
+    try:
+        op_menu = int(input("--> "))
+    except ValueError:
+        print("Ingrese un valor del 1 al 3 respectivamente")
         
+    if op_menu == 1:
+        num1 = int(input("Ingrese un número: "))
+        num2 = int(input("Ingrese otro número: "))
+        print(f"La suma de ambos números es: {fn.sumar(num1,num2)}")
 
     elif op_menu == 2:
-        print
+        num1 = int(input("Ingrese un número: "))
+        num2 = int(input("Ingrese otro número: "))
+        print(f"La resta de ambos números es: {fn.restar(num1,num2)}")     
 
     elif op_menu == 3:
-        i = False
-
+        print("saliendo")
+        menu = False 
     else:
-        print("Opción equivocada, ingrese uno de los números en las opciones")
+        print("Ingrese un valor del 1 al 3 respectivamente")
+
+
 
---
+++ casdasda.py
@@ -0,0 +1,7 @@
+def sumar(num1,num2):
+    resultado = num1 + num2
+    return resultado
+
+def restar(num1,num2):
+    resultado = num1 - num2
+    return resultado
