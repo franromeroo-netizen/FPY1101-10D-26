e7dc2288af770992a5a01ca53cec20aadbb32072
franromeroo-netizen <fran.romeroo@duocuc.cl>
2026-06-12 16:56:09 -0400

    python

---
+++ STEAM_TRABAJO/main.py
@@ -0,0 +1,33 @@
+from funciones import opciones
+menu = True
+while menu:
+    print("""======Menú catálogo de videojuegos======
+● Opción 1: Ver Catálogo de Tienda.
+● Opción 2: Agregar juego al Carrito.
+● Opción 3: Ver mi Carrito.
+● Opción 4: Cargar fondos a la Cartera.
+● Opción 5: Pagar Carrito.
+● Opción 6: Ver mi Biblioteca.
+● Opción 7: Salir.""")
+    opcion = int(input("--> "))
+
+    if opcion == 7:
+        print("Saliendo")
+        menu = False
+    elif opcion == 1:
+        print
+
+    elif opcion == 2:
+        print
+    
+    elif opcion == 3:
+        print
+
+    elif opcion == 4:
+        print
+    
+    elif opcion == 5:
+        print
+
+    elif opcion == 6:
+        print
